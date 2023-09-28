ffe97d056c6f2e229987957825b9a73cf167402e
neuefische <info@neuefische.de>
2023-09-28 15:06:33 +0200

    add CharactersOutput element

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CharactersOutput from "./CharactersOutput.tsx";
+import {characters, RickAndMortyCharacter} from "./assets/RickAndMortyCharacters.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+    const [character, setCharacter] = useState<RickAndMortyCharacter[]>(characters.results)
+
+    // function showCharacter() {
+    //     characters.filter(item => item.name)
+    //     setCharacter(character)
+    // }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <CharactersOutput allCharacters={character}/>
     </>
   )
 }
 
-export default App
+
